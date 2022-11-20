2c72ab0ba7e2e9c53368014b5e43cde41aff310a
RY3IEZN <unekue2@gmail.com>
2022-11-20 07:21:17 +0000

    hhhhhs

--- src/pages/signUp/index.jsx
+++ src/pages/signUp/index.jsx
@@ -7,12 +7,12 @@ import chevron from './chevron.svg'
 import { useForm } from "react-hook-form"
 const SignUp = () => {
   const { register, handleSubmit, formState: { errors } } = useForm()
-  
-  
+
+
   const onSubmit = (data) => {
     console.log(data)
   }
-  
+
   return (
     <div className='w-full mx-auto tablet:flex tablet:justify-center'>
       <div className=' w-full h-full bg-white tablet:w-6/12 tablet:p-3.5 mx-auto '>
@@ -26,51 +26,51 @@ const SignUp = () => {
           </div>
         </header>
         <div className='mt-10 tablet:mt-14 px-4 w-full max-w-md mx-auto'>
-         
+
           <h2 className='font-medium text-xl text-[#717172] tablet:text-4xl'>Welcome!</h2>
           <p className='mt-2 text-[#424245] text-base tablet:text-xl'>Sign up here! Please enter your details</p>
           <form onSubmit={handleSubmit(onSubmit)} className=' mt-7 text-[#4B4B4C] font-normal [&>input]:mt-2 [&>input]:w-full [&>input]:mb-3.5'>
-            
+
             <div className='relative w-full mb-4'>
               <label className='pt-3.5' htmlFor="email">Email</label><br />
-              
-              <input 
-              style={{border: errors.email ? '1px solid red': '1px solid #D0D5DD'}}
-              className='focus:outline-none mt-2 w-full h-11 p-3.5 rounded-lg' type="text" name="email" placeholder="Enter your email"
-              {...register("email", 
-              {required: true, pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ })}
+
+              <input
+                style={{ border: errors.email ? '1px solid red' : '1px solid #D0D5DD' }}
+                className='focus:outline-none mt-2 w-full h-11 p-3.5 rounded-lg' type="text" name="email" placeholder="Enter your email"
+                {...register("email",
+                  { required: true, pattern: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ })}
               />
-              {errors.email && <p className='right-0 bottom-[-37px] italic absolute text-sm mb-4' style={{color: 'red'}}>Please enter a valid email</p>}
+              {errors.email && <p className='right-0 bottom-[-37px] italic absolute text-sm mb-4' style={{ color: 'red' }}>Please enter a valid email</p>}
             </div>
 
 
             <div className='relative w-full mb-4'>
               <label htmlFor="password">Passsword</label><br />
 
-              <input 
-              style={{border: errors.password ? '1px solid red': '1px solid #D0D5DD'}}
-              className='focus:outline-none mt-2 w-full h-11 p-3.5 rounded-lg'  type="password" name="password" placeholder="Please enter your unique password"
-              {...register("password", {required: true})}/>
-              {errors.password && <p className='right-0 bottom-[-37px] italic absolute text-sm mb-4' style={{color: 'red'}}>Password must not be empty</p>}
+              <input
+                style={{ border: errors.password ? '1px solid red' : '1px solid #D0D5DD' }}
+                className='focus:outline-none mt-2 w-full h-11 p-3.5 rounded-lg' type="password" name="password" placeholder="Please enter your unique password"
+                {...register("password", { required: true })} />
+              {errors.password && <p className='right-0 bottom-[-37px] italic absolute text-sm mb-4' style={{ color: 'red' }}>Password must not be empty</p>}
             </div>
-          
+
 
             <button className=' transition ease-in duration-200 hover:bg-[#66A3FF] mt-4 text-white bg-[#0056D6] w-full h-11 rounded-lg' type="submit">Create a free account</button>
 
-            </form>
+          </form>
 
-            <p className='my-2.5 text-center text-[#0056D6]' >Or</p>
+          <p className='my-2.5 text-center text-[#0056D6]' >Or</p>
 
-            <button className='flex justify-center items-center font-medium text-[#344054] w-full  border border-[#D0D5DD] h-11 p-2 rounded-lg'>
-              <img className='mr-2 w-6' src={google} alt="google logo"/>
-               Sign Up with Google
-            </button>
+          <button className='flex justify-center items-center font-medium text-[#344054] w-full  border border-[#D0D5DD] h-11 p-2 rounded-lg'>
+            <img className='mr-2 w-6' src={google} alt="google logo" />
+            Sign Up with Google
+          </button>
 
-            <p className=' my-8 font-normal text-center text-[#0056D6]'>Have an account already? Sign in for free</p>
-             
+          <p className=' my-8 font-normal text-center text-[#0056D6]'>Have an account already? Sign in for free</p>
+
         </div>
       </div>
-      <div className="hidden tablet:block tablet:w-6/12 h-[670px] bg-cover bg-gray-100" style={{backgroundImage: `linear-gradient(115deg, rgba(0,0,0,0.1), rgba(0,0,0,0.1)), url(${image})`}}></div>
+      <div className="hidden tablet:block tablet:w-6/12 h-[670px] bg-cover bg-gray-100" style={{ backgroundImage: `linear-gradient(115deg, rgba(0,0,0,0.1), rgba(0,0,0,0.1)), url(${image})` }}></div>
     </div>
   )
 }
